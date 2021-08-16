ee9f2643dbe1aa63992a58424840f71ba1b64c96
Rohit Gupta <17207764rg@gmail.com>
2021-08-17 01:55:37 +0530

    Added S3 Bucket

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ import googleAuthConfig from "./config/google.config";
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
+import Image from "./API/Image";
 
 
 
@@ -42,6 +43,7 @@ googleAuthConfig(passport);
 zomato.use("/auth",Auth);
 zomato.use("/restaurant",Restaurant);
 zomato.use("/food",Food);
+zomato.use("/image",Image);
 
 
 zomato.get("/",(req,res)=> res.json(
