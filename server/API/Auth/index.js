767d54edd00bc343b38ef42d3d467862240b925a
Rohit Gupta <17207764rg@gmail.com>
2021-08-18 00:20:02 +0530

    Added auth validation

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -7,6 +7,9 @@ import passport from "passport";
 //Models
 import { UserModel } from "../../database/user";
 
+//validation
+import { ValidateSignup,ValidateSignin } from "../../validation/auth";
+
 
 const Router=express.Router();
 
@@ -18,9 +21,10 @@ access        public
 Method        Post
 */
 
-Router.post("/signup",async (req,res)=>{
+Router.post("/signup",async (req,res)=>{ 
     try
     {
+       await ValidateSignup(req.body.credentials);
        await UserModel.findByEmailAndPhone(req.body.credentials);
 
        //save to DB
@@ -54,6 +58,8 @@ Method        Post
 Router.post("/signin",async (req,res)=>{
     try
     {
+       await ValidateSignin(req.body.credentials);
+
       const user = await UserModel.findByEmailAndPassword(req.body.credentials);
 
        //generate JWT Auth Token
