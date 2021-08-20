713f613c12be935544023d9826212071c6c0c4c5
Rohit Gupta <17207764rg@gmail.com>
2021-08-20 23:26:47 +0530

    Added Food tab UI

--- client/src/Layout/Home.layout.jsx
+++ client/src/Layout/Home.layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //Components
 import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
 
 
 const HomeLayout = (props) =>
@@ -10,14 +11,13 @@ const HomeLayout = (props) =>
 
     <> 
 
-     <Navbar/>
-   
-   <div className="container mx-auto px-4 lg:px-20 ">
+   <Navbar/>
 
-       
-     {props.children}
-          
-     </div>
+   <div className="container mx-auto px-4 lg:px-20 ">  
+     {props.children}   
+   </div>
+
+   <FoodTab/>  
     
     </>
 
